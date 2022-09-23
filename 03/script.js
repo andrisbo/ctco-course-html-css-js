87659062f203ccac33c2421a6d143af5880c571b
andrisbo <100278395+andrisbo@users.noreply.github.com>
2022-09-23 21:09:08 +0000

    1st home work

--- 03/script.js
+++ 03/script.js
@@ -1,12 +1,12 @@
 const name = 'Java';
 
 function displayName() {
-  const name = 'Script';
-  logMessage(name);
+    const name = 'Script';
+    logMessage(name);
 }
 
 function logMessage(name) {
-  console.log(`Hello ${name}`);
+    console.log(`Hello ${name}`);
 }
 
 displayName();
@@ -16,6 +16,21 @@ linkElement.setAttribute('target', '_blank');
 
 document.querySelector('h1').style.color = '#5cdc5f';
 
-document.addEventListener('click', function(event) {
-  console.log(`Click detected x ${event.offsetX} y ${event.offsetY}`);
+document.addEventListener('click', function (event) {
+    console.log(`Click detected x ${event.offsetX} y ${event.offsetY}`);
 });
+
+//------------------------------------------------------------------------------------
+linkElement.addEventListener('click', () => {
+    linkElement.style.display = 'none';
+});
+
+let counter = 0;
+document.addEventListener('click', () => {
+    counter++;
+    if (counter == 5) {
+        alert("5 clicks done");
+    }
+});
+
+document.getElementById("title").innerHTML = new Date().getFullYear() + " TIME TO BUILD YOUR CAREER";
